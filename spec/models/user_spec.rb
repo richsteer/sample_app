c19453a7e67197119247a01c232340ce73c3371d
Richard Steer <richsteer@gmail.com>
2012-12-03 22:20:33 +0000

    add ssl to production mid way

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,11 +23,12 @@ describe User do
   it { should respond_to(:email) }
   it { should respond_to(:password_digest)}
   it { should respond_to(:password_confirmation)}
+  it { should respond_to(:remember_token)}
   it { should respond_to(:authenticate)}
 
   it { should be_valid }
 
-  describe "when name is not present" do
+   describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
   end
@@ -99,6 +100,11 @@ describe User do
       it { should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
     end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 
 end
