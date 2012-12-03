316f6e5964ab34a4e298f3805692ade483695002
Richard Steer <richsteer@gmail.com>
2012-12-03 22:20:33 +0000

    add ssl to production mid way

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,8 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
+  private
+  	def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
